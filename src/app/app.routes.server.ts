2445ba287a604fef764d8c50a5a363bcd5bb8f3e
Triantafilo <dimitri.triantafilo@accusys.com.ar>
2025-07-19 13:34:19 -0300

    feat: added routes to navigate through routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,15 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { Routes } from '@angular/router';
+import { generateSlug } from './app.routes';
+import { Project } from './app.component';
+import { projects } from './app.config';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+export const serverRoutes: Routes = [
+  ...projects.map((project: Project) => ({
+    path: generateSlug(project.name),
+    loadComponent: () =>
+      import('./@components/project-detail/project-detail.component').then(
+        (m) => m.ProjectDetailComponent
+      ),
+    data: { project },
+  })),
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,54 @@
 import { Routes } from '@angular/router';
+// import { ProjectDetailComponent } from './project-detail/project-detail.component';
+// import { HomeComponent } from './home/home.component';
+import { Project } from './app.component';
+import { projects } from './app.config';
+import { ProjectListComponent } from './@components/project-list/project-list.component';
+import { NosotrosComponent } from './@components/nosotros/nosotros.component';
+import { HomeComponent } from './@components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    // children: [
+    //   ...projects.map((project: Project) => ({
+    //     path: generateSlug(project.name),
+    //     // component: ProjectDetailComponent,
+    //     data: { project },
+    //   })),
+    // ],
+  },
+  {
+    path: 'proyectos',
+    component: ProjectListComponent,
+    // children: [
+    //   ...projects.map((project: Project) => ({
+    //     path: generateSlug(project.name),
+    //     // component: ProjectDetailComponent,
+    //     data: { project },
+    //   })),
+    // ],
+  },
+  {
+    path: 'estudio',
+    component: NosotrosComponent,
+  },
+  //   ...projects?.map((project: Project) => ({
+  //     path: generateSlug(project.name),
+  //     // component: ProjectDetailComponent,
+  //     data: { project },
+  //   })),
+
+  { path: '**', redirectTo: '' },
+];
+
+export function generateSlug(name: string): string {
+  return name
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
